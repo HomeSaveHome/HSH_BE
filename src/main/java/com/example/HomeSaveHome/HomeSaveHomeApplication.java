259b5b93541afc96f89bff50e267aa6ada3e3cd8
wldy4627 <wldy4627@gmail.com>
2025-01-26 20:20:14 +0900

    BE 초기 세팅

---
+++ src/main/java/com/example/HomeSaveHome/HomeSaveHomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.HomeSaveHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeSaveHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeSaveHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HomeSaveHome/HomeSaveHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HomeSaveHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeSaveHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
