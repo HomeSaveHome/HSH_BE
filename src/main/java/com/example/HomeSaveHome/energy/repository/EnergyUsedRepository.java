6d3213c23a79bbc613df5124e30a843f67a575aa
wldy4627 <wldy4627@gmail.com>
2025-02-11 00:58:48 +0900

    feat: 에너지 사용량 조회(by energyId)

--- src/main/java/com/example/HomeSaveHome/energy/repository/EnergyUsedRepository.java
+++ src/main/java/com/example/HomeSaveHome/energy/repository/EnergyUsedRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface EnergyUsedRepository extends JpaRepository<EnergyUsed, Long> {
+    List<EnergyUsed> findByUserIdAndEnergy_Id(Long userId, Long energyId);
 }
