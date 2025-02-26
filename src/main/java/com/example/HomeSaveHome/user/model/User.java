c8e6a2e2dea663e81d464350cbcc71d8ebe95693
lshwa <sw092311@cau.ac.kr>
2025-02-26 11:47:48 +0900

    lshwa1 작업

--- src/main/java/com/example/HomeSaveHome/user/model/User.java
+++ src/main/java/com/example/HomeSaveHome/user/model/User.java
@@ -4,6 +4,8 @@ public class User {
     private String username;
     private String email;
     private String password;
+    private int point;  // 포인트 필드 추가
+    private int level;  // 레벨 필드 추가
 
     // getters and setters
     public String getUsername() {
@@ -28,5 +30,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public int getPoint() {  // 포인트를 가져오는 메서드
+        return point;
+    }
+
+    public void setPoint(int point) {  // 포인트를 설정하는 메서드
+        this.point = point;
+    }
+
+    public int getLevel() {  // 레벨을 가져오는 메서드
+        return level;
+    }
+
+    public void setLevel(int level) {  // 레벨을 설정하는 메서드
+        this.level = level;
     }
 }
